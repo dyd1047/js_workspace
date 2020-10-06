62e12b80904bceaf518fa480ebdf2a645d377854
YONG\msi <dyd414646@gmail.com>
2020-10-06 17:49:35 +0900

    October 6, 2020

--- project1005/Bullet.js
+++ project1005/Bullet.js
@@ -24,6 +24,23 @@ class Bullet{
     tick(){
         // 총알의 물리량 변화
         this.y += this.velY;
+
+        // 총알이 한걸음, 한걸음 진행할 때 마다 배열에 존재하는 모든 적군을 대상으로
+        // 충돌했는지 여부를 판단할것임
+        for(var i = 0; i < enemyArray.length; i++){
+            var result = collisionCheck(this.img, enemyArray[i].img);
+            if (result) {
+                // 적군 배열에서 요소 삭제(splice(index, 계수)), 화면에서도 삭제(removeChild)
+                var img = enemyArray[i].img;
+                wrapper.removeChild(img);
+                enemyArray.splice(0, 1);
+
+                // 총알배열에서 요소 삭제(splice(index, 계수)), 화면에서도 삭제(removeChild)
+                wrapper.removeChild(this.img); // 화면에서 제거
+                var index = bulletArray.indexOf(this); // 현재 총알의 인스턴스가 배열의 몇번째에 존재하는지
+                bulletArray.splice(index, 1);
+            }
+        }
     }
 
     render(){
